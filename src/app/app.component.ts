f39b2f036b5af3b55e77235e79415902445d2fe1
chaitanya chorawala <chorawala.chaitany@hotmail.com>
2023-11-26 22:14:28 +0530

    Added drap-drop task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { Task } from './task/Task';
+import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ng-firebase-app';
+  todo: Task[] = [
+    {
+      title: 'Task1',
+      description: 'Task1 description',
+    },
+    {
+      title: 'Task2',
+      description: 'Task2 description',
+    },
+  ];
+  inProgess: Task[] = [];
+  done: Task[] = [];
+
+  editTask(list: string, task: Task): void {}
+
+  drop(event: CdkDragDrop<Task[]>): void {
+    if (event.previousContainer === event.container) {
+      return;
+    }
+    if (!event.container.data || !event.previousContainer.data) {
+      return;
+    }
+
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
+  }
 }
